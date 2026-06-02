cd588960656d784ee2d58160fe0e70199b772a5b
Danko Misraim Morales Loza <325326306@alumno.enp.unam.mx>
2026-06-02 12:55:03 -0600

    fix(variables): Se modifica el scope de las variables

--- dynamics/caidaLibre.php
+++ dynamics/caidaLibre.php
@@ -1,12 +1,11 @@
 <?php         
 const gravedad = 32;
-$tablaTDV = [];
+
 //__________________________________________________
 // Cálculo de distancia
 // _________________________________________________
 function calcDistancia($tiempo, $veli){
-    global $gravedad;
-    $dist = $veli*$tiempo+0.5* $gravedad * $tiempo ** 2;
+    $dist = $veli*$tiempo+0.5*gravedad * $tiempo ** 2;
     return $dist;
 }
 
@@ -14,8 +13,7 @@ function calcDistancia($tiempo, $veli){
 // Cálculo de velocidad
 // _________________________________________________
 function calcVelocidad($tiempo, $veli){
-    global $gravedad;
-    $velf = $veli+$gravedad*$tiempo;
+    $velf = $veli+gravedad*$tiempo;
     return $velf;
 }
 
@@ -23,16 +21,16 @@ function calcVelocidad($tiempo, $veli){
 // Crea tabla que contiene tiempo, distancia y velocidad final
 // _________________________________________________
 function generaTabla(){
+    global $tablaTDV;
     $tiempo = 0;
     for ($t = 1; $t <= 10; $t++){
-        $tiempo=$tiempo + 1.0;
-        global $tablaTDV; 
+        $tiempo=$tiempo + 1.0; 
         $tablaTDV[$t-1][0] = $tiempo;
         $tablaTDV[$t-1][1] = calcDistancia($tablaTDV[$t-1][0], 0);
         $tablaTDV[$t-1][2] = calcVelocidad($tablaTDV[$t-1][0], 0);
     }
-}
 
+}
 $col = 0;
 $t = 0;
 generaTabla();
